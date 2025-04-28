79f46a4010aed4dee45c4df911901bbbc5c9478a
Olivier PERDRIX <olivierperdrix@live.fr>
2025-04-28 12:23:21 +0200

    Initialisation de TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: Number(process.env.DATABASE_PORT || '33061'),
+      username: process.env.DATABASE_USER || 'myuser',
+      password: process.env.DATABASE_PASSWORD || 'mypassword',
+      database: process.env.DATABASE_NAME || 'qvema',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
