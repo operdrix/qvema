fabb72fa3a51162cd2cfc621c975cfe6b2afb395
Olivier PERDRIX <olivierperdrix@live.fr>
2025-04-29 14:24:22 +0200

    Initialisation du auth module

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Global, Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+
+@Global()
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: parseInt(
+            configService.get<string>('JWT_EXPIRATION') ?? '3600',
+          ),
+        },
+      }),
+    })
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+
+export class AuthModule { }
