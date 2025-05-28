db6af1700f49428ea861c6a646bf08e28763c6b8
Olivier PERDRIX <olivierperdrix@live.fr>
2025-05-28 11:07:36 +0200

    * refactor(typeorm.ts): remove console.log statement for environment variables

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -2,14 +2,6 @@ import { User } from 'src/modules/users/entities/user.entity';
 import { DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 
-
-console.log('Environment variables:', {
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USER,
-  DB_NAME: process.env.DB_NAME,
-});
-
 const config: DataSourceOptions & SeederOptions = {
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
